77e7c312971e87994a1ef6a1db732ee9aa3606ea
Edmar Paulino <edmarpaulino9@gmail.com>
2022-01-08 19:30:43 -0300

    class 02 - variables and operators

--- javascript/introducao-javascript-inicial/introducao-javascript/js/principal.js
+++ javascript/introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -1,3 +1,26 @@
-
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var peso = paciente.querySelector(".info-peso").textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+
+var imc = peso / (altura * altura);
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+	pesoEhValido = false;
+	paciente.querySelector(".info-imc").textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+	alturaEhValida = false;
+	paciente.querySelector(".info-imc").textContent = "Altura inválida!";
+}
+
+if (pesoEhValido && alturaEhValida) {
+	paciente.querySelector(".info-imc").textContent = imc;
+}
