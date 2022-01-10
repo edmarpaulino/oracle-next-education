f576cf864f304f64380e6887d53f6f1d59a10aa3
Edmar Paulino <edmarpaulino9@gmail.com>
2022-01-10 08:24:38 -0300

    class 03 - arrays loops and styles

--- javascript/introducao-javascript-inicial/introducao-javascript/js/principal.js
+++ javascript/introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -1,26 +1,37 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+// var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var peso = paciente.querySelector(".info-peso").textContent;
-var altura = paciente.querySelector(".info-altura").textContent;
+for (var i = 0; i < pacientes.length; i++) {
 
-var imc = peso / (altura * altura);
+	var paciente = pacientes[i];
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+	var peso = paciente.querySelector(".info-peso").textContent;
+	var altura = paciente.querySelector(".info-altura").textContent;
 
-if (peso <= 0 || peso >= 1000) {
-	pesoEhValido = false;
-	paciente.querySelector(".info-imc").textContent = "Peso inválido!";
+	var imc = peso / (altura * altura);
+
+	var pesoEhValido = true;
+	var alturaEhValida = true;
+
+	if (peso <= 0 || peso >= 1000) {
+		pesoEhValido = false;
+		paciente.querySelector(".info-imc").textContent = "Peso inválido!";
+		// adiciona uma nova classe ao elemento
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (altura <= 0 || altura >= 3.00) {
+		alturaEhValida = false;
+		paciente.querySelector(".info-imc").textContent = "Altura inválida!";
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (pesoEhValido && alturaEhValida) {
+		paciente.querySelector(".info-imc").textContent = imc.toFixed(2);
+	}
+
 }
 
-if (altura <= 0 || altura >= 3.00) {
-	alturaEhValida = false;
-	paciente.querySelector(".info-imc").textContent = "Altura inválida!";
-}
-
-if (pesoEhValido && alturaEhValida) {
-	paciente.querySelector(".info-imc").textContent = imc;
-}
